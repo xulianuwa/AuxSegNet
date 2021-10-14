deeb8ace88eae03fbc33c18211f171601e21f7b9
xulianuwa <lian.xu@uwa.edu.au>
2021-10-14 19:10:40 +0800

    add bash file

---
+++ iter_learn.sh
@@ -0,0 +1,8 @@
+python main.py --init_weights   \
+               --image_path   \
+               --init_salpgt_path  \
+               --init_segpgt_path  \
+               --model_path  \
+               --sal_pgt_path  \
+               --seg_pgt_path  \
+               --seg_output_path
